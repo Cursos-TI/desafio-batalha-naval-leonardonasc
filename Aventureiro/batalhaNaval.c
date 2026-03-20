ce0e8914bc1dc0abffe73c3a6dff04d7745643b9
Leonardo <leonardo.nasmt@gmail.com>
2026-03-20 15:19:21 -0300

    add: nivel aventureiro

--- Aventureiro/batalhaNaval.c
+++ Aventureiro/batalhaNaval.c
@@ -4,7 +4,8 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
 
-int main() {
+int main()
+{
     // Nível Novato - Posicionamento dos Navios
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
@@ -15,26 +16,80 @@ int main() {
     // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
     // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+    char linha[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+    int tabuleiro[10][10];
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+    printf(" ");
+    for (int i = 0; i < 10; i++)
+    {
+        printf(" %c", linha[i]);
+        for (int j = 0; j < 10; j++)
+        {
+            tabuleiro[i][j] = 0;
+        }
+    }
+    printf("\n");
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+    // adicionando os navios
+
+    // horizontal
+    if (tabuleiro[0][1] == 0 && tabuleiro[0][2] == 0 && tabuleiro[0][3] == 0)
+    {
+        tabuleiro[0][1] = 3;
+        tabuleiro[0][2] = 3;
+        tabuleiro[0][3] = 3;
+    }
+    else
+    {
+        printf("posicao já atribuida em [%d] e [%d]\n", 0, 1);
+    }
+
+    // vertical
+    if (tabuleiro[2][5] == 0 && tabuleiro[3][5] == 0 && tabuleiro[4][5] == 0)
+    {
+        tabuleiro[2][5] = 3;
+        tabuleiro[3][5] = 3;
+        tabuleiro[4][5] = 3;
+    }
+    else
+    {
+        printf("posicao já atribuida em [%d] e [%d]\n", 2, 5);
+    }
+
+    // diagonal
+    if (tabuleiro[5][0] == 0 && tabuleiro[6][1] == 0 && tabuleiro[7][2] == 0)
+    {
+        tabuleiro[5][0] = 3;
+        tabuleiro[6][1] = 3;
+        tabuleiro[7][2] = 3;
+    }
+    else
+    {
+        printf("posicao já atribuida em [%d] e [%d]\n", 5, 0);
+    }
+
+    // diagonal 2
+    if (tabuleiro[8][4] == 0 && tabuleiro[7][5] == 0 && tabuleiro[6][6] == 0)
+    {
+        tabuleiro[8][4] = 3;
+        tabuleiro[7][5] = 3;
+        tabuleiro[6][6] = 3;
+    }
+    else
+    {
+        printf("posicao já atribuida em [%d] e [%d]\n", 8, 4);
+    }
+
+    // impressao do tabuleiro
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d ", i);
+        for (int j = 0; j < 10; j++)
+        {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
